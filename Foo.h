425b42782ccaa2eb4b0857a700c96fa0d30c9082
Roger Whitney <whitney@cs.sdsu.edu>
2011-12-01 19:15:00 -0800

    Test commir

---
+++ Foo.h
@@ -0,0 +1,13 @@
+//
+//  Foo.h
+//  TapsGestures
+//
+//  Created by Roger Whitney on 12/1/11.
+//  Copyright (c) 2011 San Diego State University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Foo : NSObject
+
+@end
---
+++ Foo.m
@@ -0,0 +1,13 @@
+//
+//  Foo.m
+//  TapsGestures
+//
+//  Created by Roger Whitney on 12/1/11.
+//  Copyright (c) 2011 San Diego State University. All rights reserved.
+//
+
+#import "Foo.h"
+
+@implementation Foo
+
+@end
